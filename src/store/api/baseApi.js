e169dc788a3f2cc0a11a40ccf1455090bfb90248
Shukurulla <shukurullatursinbayev978@gmail.com>
2025-09-16 12:14:16 +0500

    start

--- src/store/api/baseApi.js
+++ src/store/api/baseApi.js
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+    baseUrl:
+      import.meta.env.VITE_API_URL || "https://dogerek-server.vercel.app/api",
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
       if (token) {
