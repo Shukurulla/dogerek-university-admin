30d02baad5faf942289eb20cfd75e34e790e6447
Shukurulla <shukurullatursinbayev978@gmail.com>
2025-09-16 12:19:43 +0500

    start

--- src/store/api/authApi.js
+++ src/store/api/authApi.js
@@ -1,7 +1,8 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_URL =
+  import.meta.env.VITE_API_URL || "https://dogerek-server.vercel.app/api";
 
 // Load auth from localStorage
 const loadAuthFromStorage = () => {
